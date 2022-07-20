dfe2fa2aef9ead7b79d3a8b49565568d601ca6cd
jose <joseandrescolmenares02@gmail.com>
2022-07-20 14:21:20 -0300

    casi

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -7,7 +7,12 @@ const Landing = () =>{
     <>
     <div className={s.conteiner}>
     
-        <section className={s.primerSeccion}>hola</section>
+        <section className={s.primerSeccion}>
+            <div className={s.div}>
+                
+            <h1 className={s.h1}>este es un BotChat que realize mientra busco mi primera trabajo  espero que los disfrute</h1>
+            </div>
+        </section> 
         <section className={s.segundaSeccion}><Contedido /></section>
         
     </div>
